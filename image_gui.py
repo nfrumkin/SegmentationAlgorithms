2d8fc576ce074d701fdc98079ea6f49027843050
nfrumkin <nfrumkin@bu.edu>
2019-12-09 11:20:50 -0500

    add paint to image gui

--- image_gui.py
+++ image_gui.py
@@ -1,19 +1,95 @@
 # GUI library
 import tkinter as tk
+from tkinter import *
+from tkinter.colorchooser import askcolor
 from PIL import Image, ImageTk
 import numpy as np
 
-root = tk.Tk()
-# splice gui into top and bottom panels
-top = tk.Frame(root)
-middle = tk.Frame(root)
-bottom = tk.Frame(root)
-top.pack(side="top")
-middle.pack(side="top")
-bottom.pack(side="bottom", fill="both", expand=True)
+class Paint(object):
 
-xCoords = []
-yCoords = []
+    DEFAULT_PEN_SIZE = 5.0
+    DEFAULT_COLOR = 'black'
+
+    def __init__(self, paint_utils_frame):
+        self.root = paint_utils_frame
+
+        self.fgd_button = Button(self.root, text='foreground label', command=self.label_fgd)
+        self.fgd_button.grid(row=0, column=0)
+
+        self.bgd_button = Button(self.root, text='background label', command=self.label_bgd)
+        self.bgd_button.grid(row=0, column=1)
+
+        # self.brush_button = Button(self.root, text='brush', command=self.use_brush)
+        # self.brush_button.grid(row=0, column=1)
+
+        # self.color_button = Button(self.root, text='color', command=self.choose_color)
+        # self.color_button.grid(row=0, column=2)
+
+        # self.eraser_button = Button(self.root, text='eraser', command=self.use_eraser)
+        # self.eraser_button.grid(row=0, column=3)
+
+        self.choose_size_button = Scale(self.root, from_=1, to=10, orient=HORIZONTAL)
+        self.choose_size_button.grid(row=0, column=4)
+
+        path = "imgs/cow.jpg"
+        img = ImageTk.PhotoImage(Image.open(path))
+        imgHeight = img.height()
+        imgWidth = img.width()
+        self.c = Canvas(self.root, bg='white', width=imgWidth, height=imgHeight)
+        self.c.create_image(self.root.winfo_x(),self.root.winfo_y(), image=img, anchor=tk.NW)
+        self.c.grid(row=1, columnspan=5)
+
+        self.setup()
+        self.root.mainloop()
+
+    def setup(self):
+        self.old_x = None
+        self.old_y = None
+        self.line_width = self.choose_size_button.get()
+        self.color = self.DEFAULT_COLOR
+        # self.eraser_on = False
+        self.active_button = self.fgd_button
+        self.c.bind('<B1-Motion>', self.paint)
+        self.c.bind('<ButtonRelease-1>', self.reset)
+
+    def use_pen(self):
+        self.activate_button(self.pen_button)
+    
+    def label_fgd(self):
+        self.color = "green"
+        self.activate_button(self.fgd_button)
+
+    def label_bgd(self):
+        self.color = "red"
+        self.activate_button(self.bgd_button)
+
+    # def use_brush(self):
+    #     self.activate_button(self.brush_button)
+
+    # def choose_color(self):
+    #     self.eraser_on = False
+    #     self.color = askcolor(color=self.color)[1]
+
+    # def use_eraser(self):
+    #     self.activate_button(self.eraser_button, eraser_mode=True)
+
+    def activate_button(self, some_button, eraser_mode=False):
+        self.active_button.config(relief=RAISED)
+        some_button.config(relief=SUNKEN)
+        self.active_button = some_button
+        # self.eraser_on = eraser_mode
+
+    def paint(self, event):
+        self.line_width = self.choose_size_button.get()
+        if self.old_x and self.old_y:
+            self.c.create_line(self.old_x, self.old_y, event.x, event.y,
+                               width=self.line_width, fill=self.color,
+                               capstyle=ROUND, smooth=TRUE, splinesteps=36)
+        self.old_x = event.x
+        self.old_y = event.y
+
+    def reset(self, event):
+        self.old_x, self.old_y = None, None
 
 def saveCoordinates(x,y):
     xCoords.append(x)
@@ -25,12 +101,13 @@ def createImage():
     for i in range(0,len(xCoords)):
         imgArray[xCoords[i], yCoords[i]] = 1
     
+    #todo: PIL.Image.fromarray()
+    # https://pillow.readthedocs.io/en/3.1.x/reference/Image.html
+    
 def motion(event):
     x, y = event.x, event.y
     print('{}, {}'.format(x, y))
-
     saveCoordinates(x,y)
-
 
 def endLabel():
     print("end labelling")
@@ -38,21 +115,34 @@ def endLabel():
     # imgHeight = panel2.winfo_reqheight()
     createImage()
 
-title = tk.Label(root, text="Manual Image Annotation: Outline foreground of image. Annotate using click-and-drag.")
-title.pack(in_=top, side="left")
+def start_gui(path):
+    root = tk.Tk()
+    # splice gui into top and bottom panels
+    top = tk.Frame(root)
+    paint_utils_frame = tk.Frame(root)
+    middle = tk.Frame(root)
+    bottom = tk.Frame(root)
+    top.pack(side="top")
+    paint_utils_frame.pack(side="top")
+    middle.pack(side="top")
+    bottom.pack(side="bottom", fill="both", expand=True)
 
-# end label button
-endButton = tk.Button(root, text="finish labelling", command=endLabel)
-endButton.pack(in_=middle, side="left")
+    xCoords = []
+    yCoords = []
 
-# bottom panel to display image
-path = "imgs/cow.jpg"
-img = ImageTk.PhotoImage(Image.open(path))
-imgHeight = img.height()
-imgWidth = img.width()
-panel2 = tk.Label(root, image = img)
-panel2.pack(in_=bottom, side = "left", fill = "both", expand = "yes")
+    title = tk.Label(root, text="Annotate foreground and background. Click buttons to begin, click \"Finish labelling\" when done")
+    title.pack(in_=top, side="left")
 
-# click-and-drage mouse event
-root.bind("<B1-Motion>", motion)
-root.mainloop()
+    # end label button
+    endButton = tk.Button(middle, text="finish labelling", command=endLabel)
+    endButton.pack(in_=middle, side="left")
+
+    Paint(paint_utils_frame)
+
+    # click-and-drage mouse event
+    root.bind("<B1-Motion>", motion)
+    root.mainloop()
+
+if __name__ == "__main__":
+    img_path = "imgs/cow.jpg"
+    start_gui(img_path)
